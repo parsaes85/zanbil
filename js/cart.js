c3cd4b73a5c89d53b714537b62691cd761cbc013
parsa <parsaes771385@gmail.com>
2023-08-05 13:18:07 +0430

    remove, increase and decrease cart product

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,8 @@
-import { showCartProducts } from "./funcs/cart.js";
+import { showCartProducts, removeCartProduct, increaseCartProductCount, decreaseCartProductCount } from "./funcs/cart.js";
+
+window.removeCartProduct = removeCartProduct
+window.increaseCartProductCount = increaseCartProductCount
+window.decreaseCartProductCount = decreaseCartProductCount
 
 window.addEventListener('load', () => {
     showCartProducts()
--- js/funcs/cart.js
+++ js/funcs/cart.js
@@ -1,5 +1,5 @@
 import { sumCartTotalPrice } from "./shared.js"
-import { getLocalStorage } from "./utils.js"
+import { getLocalStorage, removeFromCartLocalStorage, setToLocalStorage } from "./utils.js"
 
 let cartProductsArray = []
 
@@ -7,13 +7,16 @@ const showCartProducts = () => {
     const desktopCartProductsWrapper = document.getElementById('desktop-cart-products-wrapper')
     const mobileCartProductsWrapper = document.getElementById('mobile-cart-products-wrapper')
 
+    desktopCartProductsWrapper.innerHTML = ''
+    mobileCartProductsWrapper.innerHTML = ''
+
     cartProductsArray = getLocalStorage("zanbil-cart")
 
     cartProductsArray.forEach(product => {
         desktopCartProductsWrapper.insertAdjacentHTML('beforeend', `
         <tr class="border-b">
             <td class="py-4 text-base flex items-center justify-center gap-4">
-                <i class="fa fa-xmark text-gray-800 cursor-pointer"></i>
+                <i onclick='removeCartProduct(${product.id})' class="fa fa-xmark text-gray-800 cursor-pointer"></i>
                 <a href="#">
                     <img src="${product.image}" class="w-20" alt="">
                 </a>
@@ -26,9 +29,9 @@ const showCartProducts = () => {
             </td>
             <td class="py-4">
                 <div class="flex items-center border-2 rounded-md w-fit text-sm text-gray-500">
-                    <span class="px-1.5 py-1 border-l-2 transition duration-200 rounded-r-md cursor-pointer hover:bg-darkRed hover:text-white">-</span>
+                    <span onclick='decreaseCartProductCount(${product.id})' class="px-1.5 py-1 border-l-2 transition duration-200 rounded-r-md cursor-pointer hover:bg-darkRed hover:text-white">-</span>
                     <p class="px-2 py-1">${product.count}</p>
-                    <span class="px-1.5 py-1 border-r-2 transition duration-200 rounded-l-md cursor-pointer hover:bg-darkRed hover:text-white">+</span>
+                    <span onclick='increaseCartProductCount(${product.id})' class="px-1.5 py-1 border-r-2 transition duration-200 rounded-l-md cursor-pointer hover:bg-darkRed hover:text-white">+</span>
                 </div>
             </td>
             <td class="py-4 text-darkRed font-semibold text-end">
@@ -44,7 +47,7 @@ const showCartProducts = () => {
             <div class="w-full">
                 <div class="flex text-sm justify-between">
                     <p class="text-gray-900">${product.title_fa}</p>
-                    <i class="fa-solid fa-xmark pr-2"></i>
+                    <i onclick='removeCartProduct(${product.id})' class="fa-solid fa-xmark pr-2"></i>
                 </div>
                 <div class="flex text-sm justify-between border-b border-dashed py-2">
                     <p class="text-xs font-semibold">قیمت</p>
@@ -53,9 +56,9 @@ const showCartProducts = () => {
                 <div class="flex text-sm justify-between items-center border-b border-dashed py-2">
                     <p class="text-xs font-semibold">تعداد</p>
                     <div class="flex items-center border-2 rounded-md w-fit text-sm text-gray-500">
-                        <span class="px-1.5 py-1 border-l-2 transition duration-200 rounded-r-md cursor-pointer hover:bg-darkRed hover:text-white">-</span>
+                        <span onclick='decreaseCartProductCount(${product.id})' class="px-1.5 py-1 border-l-2 transition duration-200 rounded-r-md cursor-pointer hover:bg-darkRed hover:text-white">-</span>
                         <p class="px-2 py-1">${product.count}</p>
-                        <span class="px-1.5 py-1 border-r-2 transition duration-200 rounded-l-md cursor-pointer hover:bg-darkRed hover:text-white">+</span>
+                        <span onclick='increaseCartProductCount(${product.id})' class="px-1.5 py-1 border-r-2 transition duration-200 rounded-l-md cursor-pointer hover:bg-darkRed hover:text-white">+</span>
                     </div>
                 </div>
                 <div class="flex text-sm font-semibold justify-between py-2">
@@ -70,6 +73,34 @@ const showCartProducts = () => {
     showCartTotalPrice()
 }
 
+const removeCartProduct = (productId) => {
+    removeFromCartLocalStorage(productId)
+    showCartProducts()
+}
+
+const increaseCartProductCount = (productId) => {
+    cartProductsArray = getLocalStorage('zanbil-cart')
+
+    let mainCartProductIndex = cartProductsArray.findIndex(product => product.id == productId)
+
+    ++cartProductsArray[mainCartProductIndex].count
+
+    setToLocalStorage('zanbil-cart', cartProductsArray)
+    showCartProducts()
+}
+const decreaseCartProductCount = (productId) => {
+    cartProductsArray = getLocalStorage('zanbil-cart')
+
+    let mainCartProductIndex = cartProductsArray.findIndex(product => product.id == productId)
+
+    if(cartProductsArray[mainCartProductIndex].count > 1) {
+        --cartProductsArray[mainCartProductIndex].count
+    }
+
+    setToLocalStorage('zanbil-cart', cartProductsArray)
+    showCartProducts()
+}
+
 const showCartTotalPrice = () => {
     const totalPriceToPay = document.getElementById('total-price-to-pay')
     
@@ -78,4 +109,4 @@ const showCartTotalPrice = () => {
     totalPriceToPay.innerHTML = `${(sumCartTotalPrice() + 29000).toLocaleString()} تومان`
 }
 
-export { showCartProducts }
+export { showCartProducts, removeCartProduct, increaseCartProductCount, decreaseCartProductCount }
